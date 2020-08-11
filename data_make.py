07999097d321bc401d2a68a22bfe3d036ee50229
Jonghoe <ninebell94@gmail.com>
2020-08-11 13:06:18 +0900

    update

--- data_make.py
+++ data_make.py
@@ -1,11 +1,42 @@
 import os
 import numpy as np
+import sys
+import h5py
 import random
+from train import *
+import matplotlib.pyplot as plt
+from pathlib import Path
 
 root_path = 'D:\\dataset\\multiview_hand_pose_dataset_uploaded_v2'
 proj_2d_path = root_path+'\\projections_2d'
 proj_3d_path = root_path+'\\projections_3d'
 img_path = root_path+'\\augmented_samples'
+
+
+def heat_map_create(conf):
+    def joint_scale(joint_path, scale):
+        joint = read_points(joint_path)
+        joint = joint / scale
+        return joint
+
+    file_list = conf['list']
+
+    all_len = len(file_list)
+    for iter in range(all_len):
+        joint2 = joint_scale(os.path.join(proj_2d_path,file_list[iter][1]), np.asarray([640,480]))
+        joint2 = np.asarray([joint_2_heat_map(joint, (48, 64)) for joint in joint2])
+
+        for idx, heat_map in enumerate(joint2):
+            if conf['is_train']:
+                path = '{0}\\{1}'.format('D:\\dataset\\multiview_hand_pose_dataset_uploaded_v2\\train\\heat_map', idx)
+            else:
+                path = '{0}\\{1}'.format('D:\\dataset\\multiview_hand_pose_dataset_uploaded_v2\\test\\heat_map', idx)
+
+            heat_map = torch.from_numpy(heat_map)
+            path = os.path.join(path, '{0}.png'.format(file_list[iter][1].split('.')[0]))
+            save_image(heat_map, path)
+
+        print(file_list[iter][1])
 
 
 def get_fool_path(root_path):
@@ -16,24 +47,28 @@ def get_fool_path(root_path):
         file_list = os.listdir(middle_path)
         for file in file_list:
             last_path = os.path.join(middle_path, file)
-            file_path.append(last_path)
+            idx = last_path.rfind('data')
+            file_path.append(last_path[idx:])
     return file_path
 
 
-def file_save(save_path, img_path, proj_2d_path, proj_3d_path, index, beg, end):
+def file_save(save_path, img_path, index, beg, end):
     fp = open(save_path, 'w')
     for j in index[beg:end]:
         img = img_path[j]
-        proj_2d = proj_2d_path[j]
-        proj_3d = proj_3d_path[j]
-        fp.write('{0},{1},{2}\n'.format(img, proj_2d, proj_3d))
+        split = img.split('\\')
+        folder_name = split[0]
+        file_order = split[1].split('_')
+        proj_2d = '{0}\\{1}_jointsCam_{2}.txt'.format(folder_name,file_order[0], file_order[2].split('.')[0])
+        fp.write('{0},{1},{2}\n'.format(img, proj_2d, proj_2d))
     fp.close()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__2":
     img_file_path = get_fool_path(img_path)
-    proj_2d_file_path = get_fool_path(proj_2d_path)
-    proj_3d_file_path = get_fool_path(proj_3d_path)
+    sorted(img_file_path)
+
+    print(len(img_file_path))
 
     idx = np.arange(len(img_file_path))
     length = len(idx)
@@ -45,13 +80,144 @@ if __name__ == "__main__":
 
     beg = 0
     end = train_len
-    file_save('train_file_pair.csv', img_file_path, proj_2d_file_path, proj_3d_file_path, idx, beg, end)
+    file_save('train_file_pair.csv', img_file_path, idx, beg, end)
 
     beg = end
     end = beg + validate_len
-    file_save('validate_file_pair.csv', img_file_path, proj_2d_file_path, proj_3d_file_path, idx, beg, end)
+    file_save('validate_file_pair.csv', img_file_path, idx, beg, end)
 
     beg = end
     end = beg + test_len
-    file_save('test_file_pair.csv', img_file_path, proj_2d_file_path, proj_3d_file_path, idx, beg, end)
+    file_save('test_file_pair.csv', img_file_path, idx, beg, end)
+
+if __name__ == "__main__2":
+    train_file_list = file_list_load('train_file_pair.csv')
+    validate_file_list = file_list_load('validate_file_pair.csv')
+    test_file_list = file_list_load('test_file_pair.csv')
+
+    train_root_folder = os.path.join(root_path, 'train')
+    validate_root_folder = os.path.join(root_path, 'validate')
+    test_root_folder = os.path.join(root_path, 'test')
+
+    os.makedirs(train_root_folder, exist_ok=True)
+    os.makedirs(validate_root_folder, exist_ok=True)
+    os.makedirs(test_root_folder, exist_ok=True)
+
+    os.makedirs(os.path.join(train_root_folder, 'heat_map'), exist_ok=True)
+    os.makedirs(os.path.join(validate_root_folder, 'heat_map'), exist_ok=True)
+    os.makedirs(os.path.join(test_root_folder, 'heat_map'), exist_ok=True)
+
+    for j in range(21):
+        os.makedirs(os.path.join(train_root_folder, 'heat_map')+'\\{0}'.format(j), exist_ok=True)
+        os.makedirs(os.path.join(validate_root_folder, 'heat_map')+'\\{0}'.format(j), exist_ok=True)
+        os.makedirs(os.path.join(test_root_folder, 'heat_map')+'\\{0}'.format(j), exist_ok=True)
+        for i in range(1,22):
+            train_heat_map_path = os.path.join(train_root_folder, 'heat_map')+'\\{0}\\data_{1}'.format(j, i)
+            os.makedirs(train_heat_map_path, exist_ok=True)
+
+            validate_heat_map_path = os.path.join(validate_root_folder, 'heat_map') + '\\{0}\\data_{1}'.format(j, i)
+            os.makedirs(validate_heat_map_path, exist_ok=True)
+
+            test_heat_map_path = os.path.join(test_root_folder, 'heat_map') + '\\{0}\\data_{1}'.format(j, i)
+            os.makedirs(test_heat_map_path, exist_ok=True)
+
+    # heat_map_create({
+    #     'list': train_file_list,
+    #     'is_train': True
+    # })
+    # heat_map_create({
+    #     'list': validate_file_list, #     'is_train': False
+    # })
+
+    heat_map_create({
+        'list': test_file_list,
+        'is_train': False
+    })
+
+if __name__ == "__main__1":
+    with h5py.File('train_data.h5', 'r') as hf:
+        print(hf.keys())
+        print(hf['joint_3']['Joint_3_0'][:,])
+        plt.imshow(hf['joint_2']['Joint_2_0'][:,:,0])
+        plt.show()
+
+
+def h5py_table_create(name, generator, conf):
+    with h5py.File(name, 'w') as hf:
+        x_g = hf.create_group('x')
+        j_2_g = hf.create_group('joint_2')
+        j_3_g = hf.create_group('joint_3')
+        for i, (x, y) in enumerate(generator(conf)):
+            print(i)
+
+            x = np.squeeze(x)
+            x = np.moveaxis(x, 0, -1)
+
+            joint_2 = np.squeeze(y[0])
+            joint_2 = np.moveaxis(joint_2, 0, -1)
+
+            joint_3 = np.squeeze(y[1])
+            joint_3 = np.moveaxis(joint_3, 0, -1)
+
+            Xset = x_g.create_dataset(
+                name='X_'+str(i),
+                data=x,
+                shape=(192, 256, 3),
+                maxshape=(192, 256, 3),
+                compression='gzip',
+                compression_opts=9
+            )
+
+            joint_2_set = j_2_g.create_dataset(
+                name='Joint_2_'+str(i),
+                data=joint_2,
+                shape=(48,64,21),
+                maxshape=(48,64,21),
+                compression="gzip",
+                compression_opts=9
+            )
+
+            joint_3_set = j_3_g.create_dataset(
+                name='Joint_3_'+str(i),
+                data=joint_3,
+                shape=(63,),
+                maxshape=(None, ),
+                compression="gzip",
+                compression_opts=9
+            )
+
+
+if __name__ == "__main__2":
+
+    train_file_list = file_list_load('train_file_pair.csv')
+    validate_file_list = file_list_load('validate_file_pair.csv')
+    test_file_list = file_list_load('test_file_pair.csv')
+    batch_size=1
+
+    h5py_table_create('train_data.hdf5', data_generator, {
+         'root': 'D:\\dataset\\multiview_hand_pose_dataset_uploaded_v2',
+         'is_train': 0,
+         'batch': batch_size,
+         'list': train_file_list})
+
+    h5py_table_create('validate_data.hdf5', data_generator, {
+        'root': 'D:\\dataset\\multiview_hand_pose_dataset_uploaded_v2',
+        'is_train': 1,
+        'batch': batch_size,
+        'list': validate_file_list})
+
+    h5py_table_create('test_data.hdf5', data_generator, {
+        'root': 'D:\\dataset\\multiview_hand_pose_dataset_uploaded_v2',
+        'is_train': 2,
+        'batch': batch_size,
+        'list': test_file_list})
+
+if __name__ == "__main__":
+
+    hf = h5py.File('train_data.hdf5', 'r')
+
+    for x, y in data_load_from_hdf5({'hdf5': hf, 'batch_size': 32, 'is_train': True}):
+        print(x.shape)
+        print(y[0].shape, y[1].shape)
+
 
--- train.py
+++ train.py
@@ -1,12 +1,17 @@
 import os
 
 import torch
-import torch.nn as nn
+from torch_module.losses import pixel_logistic_focal_loss
+import random
+
+import matplotlib.pyplot as plt
 
 from torch_module.utils import train_model
 import numpy as np
 from PIL import Image
 from model import NDenseNet
+
+import h5py
 
 root_path = 'C:\\Users\\rnwhd\\Desktop\\git\\HandPoseWithDenseNet\\result'
 
@@ -27,13 +32,54 @@ def image_encode(image):
     return image
 
 
+def joint_2_heat_map(joint, shape):
+    joint[0] = int(joint[0] * shape[0])
+    joint[1] = int(joint[1] * shape[1])
+    base = np.zeros(shape)
+    for r in range(shape[0]):
+        for c in range(shape[1]):
+            base[r, c] = np.exp(-((joint[0]-c)**2 + (joint[1]-r)**2)/5)
+    return base
+
+
 def data_generator(conf):
-    batch = conf['batch']
-    file_list = conf['list']
     def joint_scale(joint_path, scale):
         joint = read_points(joint_path)
         joint = joint / scale
         return joint
+
+    batch = conf['batch']
+    xs = conf['x']
+    ys = conf['y']
+
+    all_len = len(xs)
+    iter_len = all_len // batch
+    for iter in range(iter_len):
+        x = []
+        y = []
+        for b in range(batch):
+            b_idx = iter*batch + b
+            x.append(xs[b_idx])
+            y.append(ys[b_idx])
+
+        x = np.asarray(x)
+        x = torch.from_numpy(x).type(torch.FloatTensor).cuda()
+
+        y = np.asarray(y)
+        y = torch.from_numpy(y).type(torch.FloatTensor).cuda()
+
+        yield x, y
+
+
+def data_generator(conf):
+    def joint_scale(joint_path, scale):
+        joint = read_points(joint_path)
+        joint = joint / scale
+        return joint
+
+    batch = conf['batch']
+    file_list = conf['list']
+    root_path = conf['root']
 
     all_len = len(file_list)
     iter_len = all_len // batch
@@ -43,27 +89,46 @@ def data_generator(conf):
         point_3d = []
         for b in range(batch):
             b_idx = iter*batch + b
-            input_img = image_encode(Image.open(file_list[b_idx][0]))
+            input_img = image_encode(Image.open(os.path.join(root_path+'\\augmented_samples', file_list[b_idx][0])))
             x.append(input_img)
-            joint2 = joint_scale(file_list[b_idx][1], np.asarray([640,480]))
-            joint3 = joint_scale(file_list[b_idx][2], np.asarray([500,500,1000]))
+
+            joint2 = []
+
+            for j in range(21):
+
+                if conf['as_train'] == 0:
+                    path = '{0}\\{1}\\{2}\\'.format('{0}'.format(root_path), 'train\\heat_map', j)
+                elif conf['is_train'] == 1:
+                    path = '{0}\\{1}\\{2}\\'.format('{0}'.format(root_path), 'validate\\heat_map', j)
+                elif conf['is_train'] == 2:
+                    path = '{0}\\{1}\\{2}\\'.format('{0}'.format(root_path), 'test\\heat_map', j)
+
+                img = Image.open(path+file_list[b_idx][1].split('.')[0]+'.png').convert('L')
+                img = np.asarray(img)
+                img = img/255
+                joint2.append(img)
+
+            joint2 = np.asarray(joint2)
+
+            joint3 = joint_scale(os.path.join(root_path+'\\projections_3d', file_list[b_idx][2]), np.asarray([500,500,1000]))
             joint3 = joint3 - joint3[-1, :]
+            joint3 = np.reshape(joint3, (63,))
             point_2d.append(joint2)
             point_3d.append(joint3)
 
         x = np.asarray(x)
-        x = torch.from_numpy(x).type(torch.FloatTensor).cuda()
 
         point_2d = np.asarray(point_2d)
-        point_2d = torch.from_numpy(point_2d).type(torch.FloatTensor).cuda()
 
         point_3d = np.asarray(point_3d)
-        point_3d = torch.from_numpy(point_3d).type(torch.FloatTensor).cuda()
         yield x, (point_2d, point_3d)
 
 
 def custom_loss(target, predict):
-    return torch.mean(torch.abs(target[0] - predict[0])) + torch.mean(torch.abs(target[1] - predict[1]))
+    shape = target[0][0,0].shape
+    pixel_loss = pixel_logistic_focal_loss(target[0], predict[0], shape=shape)
+    mse = torch.mean(torch.pow(target[1] - predict[1], 2))
+    return pixel_loss + 100*mse
 
 
 def calc_mae_px(target, result):
@@ -90,34 +155,150 @@ def file_list_load(path):
     return file_list
 
 
+def PCK_2D(target, predict):
+    with torch.no_grad():
+        t_np = target[0].cpu().numpy()
+        p_np = predict[0].cpu().numpy()
+        t_flat = np.reshape(t_np, (target[0].shape[0], 21, -1))
+        p_flat = np.reshape(p_np, (target[0].shape[0], 21, -1))
+        t_idx = np.argmax(t_flat, axis=2)
+        p_idx = np.argmax(p_flat, axis=2)
+        t_idx = np.array([t_idx//64, t_idx%64])
+        p_idx = np.array([p_idx//64, p_idx%64])
+        dist = np.power(t_idx - p_idx, 2)
+        dist = np.sqrt(dist[0]+dist[1])
+        dist = np.mean(dist < 3)
+
+    return dist
+
+
+def PCK_3D(target, predict):
+    with torch.no_grad():
+        joint_3_p = predict[1][0].cpu().detach().numpy()
+        joint_3_p = np.reshape(joint_3_p, (20, 3))
+
+        joint_3_t = target[1][0].cpu().detach().numpy()
+        joint_3_t = np.reshape(joint_3_t, (20, 3))
+
+        x_3_p = joint_3_p[:-1, 0]
+        y_3_p = joint_3_p[:-1, 1]
+        z_3_p = joint_3_p[:-1, 2]
+
+        x_3_t = joint_3_t[:-1, 0]
+        y_3_t = joint_3_t[:-1, 1]
+        z_3_t = joint_3_t[:-1, 2]
+
+        dist_p = np.sqrt(np.square(x_3_p) + np.square(y_3_p) + np.square(z_3_p))
+        dist_t = np.sqrt(np.square(x_3_t) + np.square(y_3_t) + np.square(z_3_t))
+
+        dist_percent = dist_p / dist_t
+        dist_percent = np.where(dist_percent > 0.9, 1, 0)
+        return np.mean(dist_percent)
+
+
+def data_load_from_hdf5(conf):
+    batch = conf['batch_size']
+    hdf = conf['hdf5']
+    Xs = hdf['x']
+    joint_2 = hdf['joint_2']
+    joint_3 = hdf['joint_3']
+    iter_len = len(list(Xs.keys()))
+    idxes = np.arange(iter_len)
+    if conf['is_train']:
+        random.shuffle(idxes)
+
+    iter_len = len(list(Xs.keys()))//batch
+    for iter in range(iter_len):
+        x = []
+        point_2d = []
+        point_3d = []
+        for b in range(batch):
+            b_idx = iter*batch + b
+            input_img = Xs['X_{}'.format(idxes[b_idx])][:,:,:]
+            input_img = np.moveaxis(input_img, -1, 0)
+            x.append(input_img)
+
+            j2 = joint_2['Joint_2_{}'.format(idxes[b_idx])][:,:,:]
+            j3 = joint_3['Joint_3_{}'.format(idxes[b_idx])][:-3,]
+            j2 = np.moveaxis(j2, -1, 0)
+            point_2d.append(j2)
+            point_3d.append(j3)
+
+        x = np.asarray(x)
+        x = torch.from_numpy(x).type(torch.FloatTensor).cuda()
+
+        point_2d = np.asarray(point_2d)
+        point_3d = np.asarray(point_3d)
+
+        point_2d = torch.from_numpy(point_2d).type(torch.FloatTensor).cuda()
+        point_3d = torch.from_numpy(point_3d).type(torch.FloatTensor).cuda()
+
+        yield x, (point_2d, point_3d)
+
+
+def test_model(net, pretrain_path, data_loader):
+
+    if torch.cuda.is_available():
+        net.cuda()
+
+    net.load_state_dict(torch.load(pretrain_path))
+    net.eval()
+
+    for iter, (x, y) in enumerate(data_loader['loader'](data_loader['conf'])):
+        print(x.shape)
+        result = net(x)
+
+        PCK_2D(y, result)
+        PCK_3D(y, result)
+
+        del result
+
+
 if __name__ == "__main__":
+
     net = NDenseNet()
 
     is_cuda = torch.cuda.is_available()
 
-    if is_cuda:
-        net = net.cuda()
-
     optim = torch.optim.Adam(net.parameters(), lr=1e-3)
 
-    batch_size = 64
+    batch_size = 16
 
     train_file_list = file_list_load('train_file_pair.csv')
     validate_file_list = file_list_load('validate_file_pair.csv')
 
     print(len(train_file_list), len(train_file_list)//64)
 
-
-    save_path = '.\\result'
+    save_path = 'D:\\dataset\\multiview_hand_pose_dataset_uploaded_v2\\result'
     os.makedirs(save_path, exist_ok=True)
 
-    train_model(100, net, custom_loss, optim,
-                {'loader': data_generator,
-                 'conf': {
-                     'batch': batch_size,
-                     'list': train_file_list}},
-                {'loader': data_generator,
-                 'conf': {
-                     'batch': batch_size,
-                     'list': validate_file_list}},
-                save_path, 'test', check_point)
+    criterion = custom_loss
+
+    train_hf = h5py.File('train_data.hdf5', 'r')
+    test_hf = h5py.File('test_data.hdf5', 'r')
+    validate_hf = h5py.File('validate_data.hdf5', 'r')
+
+    is_train = True
+
+    if is_train:
+        train_model(500, net, custom_loss, optim,
+                {'loader': data_load_from_hdf5,
+                 'conf': {'hdf5': train_hf, 'batch_size': batch_size, 'is_train': True}
+                 },
+                {'loader': data_load_from_hdf5,
+                 'conf': {'hdf5': validate_hf, 'batch_size': batch_size, 'is_train': True}
+                 },
+                save_path, 'test', check_point, accuracy=[
+                    {'metrics': PCK_2D, 'name': 'PCK_2D'},
+                    {'metrics': PCK_3D, 'name': 'PCK_3D'}
+                 ]
+                )
+
+    if not is_train:
+        pretrain_path = 'result\\0.09887924045324326\\model.dict'
+        test_model(net, pretrain_path,
+               {'loader': data_load_from_hdf5,
+                 'conf': {'hdf5': test_hf, 'batch_size': 1, 'is_train': True}
+                 })
+
+
