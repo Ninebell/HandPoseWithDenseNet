21689bf2b610a5df6e2fb97b2ebf171d9a491aed
Ninebell <ninebell94@gmail.com>
2020-08-20 13:55:19 +0900

    update

--- model.py
+++ model.py
@@ -19,41 +19,139 @@ class DenseUNetFilter(nn.Module):
         self.conv = Conv2D(3, self.growth_k*2, 7, 2, 3, self.activation, True)
         self.conv3 = Conv2D(self.growth_k*2, self.growth_k*2, 3, 1, 1, activation=self.activation, batch=True)
         self.max_pool = nn.MaxPool2d(3, stride=2, padding=1)
-        self.down_dense_list = nn.ModuleList()
-        self.base_connection = nn.ModuleList()
-        self.down_pool_list = nn.ModuleList()
-        input_ch = self.growth_k*2
 
-        for j in range(len(self.dense_count)):
+        self.down_dense_list = nn.ModuleList()
+        self.down_pool_list = nn.ModuleList()
+
+        self.up_dense_list = nn.ModuleList()
+        self.up_pool_list = nn.ModuleList()
+
+        self.base_connection = nn.ModuleList()
+        self.down_connect_list = nn.ModuleList()
+        self.up_connect_list = nn.ModuleList()
+
+        model_depth = len(self.dense_count)
+        input_ch_list = [self.growth_k*2]
+        # input_ch = self.growth_k*2
+
+        ##########################################################################
+        # Down sampling Region
+        ##########################################################################
+
+        for j in range(model_depth):
+            input_ch = input_ch_list[-1]
             dense_block = DenseBlock(input_ch, self.growth_k, self.dense_count[j])
             self.down_dense_list.append(dense_block)
 
             output_ch = dense_block.out_ch
 
-            if j != len(self.dense_count)-1:
-                down_pool_layer = nn.Sequential(
-                    Conv2D(output_ch, output_ch, 1, 1, 0, self.activation,True),
-                    nn.AvgPool2d(2, stride=2, padding=0)
-                )
-                self.down_pool_list.append(down_pool_layer)
-            input_ch = output_ch
+            down_pool_layer = nn.Sequential(
+                Conv2D(output_ch, output_ch, 1, 1, 0, self.activation,True),
+                nn.AvgPool2d(2, stride=2, padding=0)
+            )
+            self.down_pool_list.append(down_pool_layer)
 
-            base_connect = BottleNeckBlock(dense_block.out_ch, True, activation=self.activation)
+            input_ch_list.append(output_ch)
 
-            self.base_connection.append(base_connect)
+            if j != len(self.dense_count)-1 and self.using_down:
+                self.down_connect_list.append(nn.Sequential(
+                    nn.AvgPool2d(2,2,0),
+                    BottleNeckBlock(output_ch, True, activation=self.activation)
+                ))
+
+            if j != 0 and self.using_up:
+                self.up_connect_list.append(nn.Sequential(
+                    nn.Upsample(scale_factor=2,mode='nearest'),
+                    BottleNeckBlock(output_ch, True, activation=self.activation)
+                ))
+
+            self.base_connection.append(BottleNeckBlock(output_ch, True, activation=self.activation))
+        input_ch = input_ch_list[-1]
+        self.middle_block = nn.Sequential(
+            BottleNeckBlock(input_ch, True, activation=self.activation),
+            BottleNeckBlock(input_ch, True, activation=self.activation),
+        )
+
+        print(input_ch_list)
+
+        ##########################################################################
+        # Up sampling Region
+        ##########################################################################
+        for j in range(model_depth):
+            up_pool_layer = UpConv2D(2, input_ch, input_ch, 3, 1, 1, activation=self.activation, batch=True)
+            self.up_pool_list.append(up_pool_layer)
+            print(j, input_ch)
+            input_ch = input_ch + input_ch_list[-j-1]
+            if j != model_depth-1 and self.using_down:
+                input_ch = input_ch + input_ch_list[-j-2]
+            if j != 0 and self.using_up:
+                input_ch = input_ch + input_ch_list[-j]
+            print(j, input_ch)
+
+            dense_block = DenseBlock(input_ch, self.growth_k, self.dense_count[len(self.dense_count) - j - 1])
+            self.up_dense_list.append(nn.Sequential(
+                dense_block,
+                Conv2D(dense_block.out_ch, input_ch_list[-j-2], 3, 1, 1, activation=self.activation, batch=True)
+            ))
+            input_ch = input_ch_list[-j-2]
+        self.joint_2d_b = BottleNeckBlock(input_ch, attention=True, activation=self.activation)
+        self.joint_2d = Conv2D(input_ch, 21, 1, 1, 0, 'sigmoid')
+
+        self.joint_3d_b = BottleNeckBlock(input_ch, attention=True, activation=self.activation)
+        self.joint_3d_l = nn.Sequential(
+            nn.Linear(input_ch, 128),
+            nn.BatchNorm1d(128)
+        )
+        self.joint_3d = nn.Linear(128, 60)
 
     def forward(self, x):
         x = self.conv(x)
         x = self.max_pool(self.conv3(x))
 
-        # down sampling
-        for j in range(len(self.dense_count)):
-            print(x.shape)
+        model_depth = len(self.dense_count)
+        down_connect = []
+        up_connect = []
+        base_connect = []
+        # Down sampling
+        for j in range(model_depth):
             x = self.down_dense_list[j](x)
-            if j != len(self.dense_count)-1:
-                x = self.down_pool_list[j](x)
+            base = self.base_connection[j](x)
+            base_connect.append(base)
+            if j != model_depth-1 and self.using_down:
+                down = self.down_connect_list[j](x)
+                down_connect.append(down)
 
-        return x
+            if j != 0 and self.using_up:
+                up = self.up_connect_list[j-1](x)
+                up_connect.append(up)
+            x = self.down_pool_list[j](x)
+        x = self.middle_block(x)
+
+        down_count = -1
+        up_count = -1
+        for j in range(model_depth):
+            up_base = self.up_pool_list[j](x)
+            connect = torch.cat((up_base, base_connect[model_depth - j - 1]),dim=1)
+            if j != model_depth-1 and self.using_down:
+                connect = torch.cat((connect, down_connect[down_count]), dim=1)
+                down_count = down_count-1
+            if j != 0 and self.using_up:
+                connect = torch.cat((connect, up_connect[up_count]), dim=1)
+                up_count = up_count-1
+            x = self.up_dense_list[j](connect)
+
+        print(down_count, up_count, len(down_connect))
+
+        joint_2d = self.joint_2d_b(x)
+        joint_2d = self.joint_2d(joint_2d)
+
+        joint_3d = self.joint_3d_b(x)
+        joint_3d = torch.mean(joint_3d, dim=[2, 3])
+        joint_3d = joint_3d.view((joint_3d.shape[0], -1))
+        joint_3d = torch.relu(self.joint_3d_l(joint_3d))
+        joint_3d = torch.tanh(self.joint_3d(joint_3d))
+
+        return joint_2d, joint_3d
 
 
 class DenseUNet(nn.Module):
@@ -178,26 +276,16 @@ class DenseUNet(nn.Module):
 
 
 if __name__ == "__main__":
-    net = DenseUNetFilter()
+    net = DenseUNetFilter(using_down=True, using_up=True)
     print('{:,}'.format(get_param_count(net)))
-    t = torch.rand((64, 3, 192, 256))
-    o = torch.rand((1,21,64,64))
+    t = torch.rand((4, 3, 192, 256))
+    o = torch.rand((4,21,64,64))
 
     result = net(t)
 
     optim = torch.optim.Adam(net.parameters(),lr=1e-4)
     criterion = torch.nn.MSELoss()
 
-    # for i in range(100):
-    #     optim.zero_grad()
-    #     result = net(t)
-    #     print(torch.mean(torch.abs(result-o)))
-    #     loss = criterion(o, result)
-    #     loss.backward()
-    #     optim.step()
-    #     # for p in net.parameters():
-    #     #     print(p)
-    #     #     break
     print(result[0].shape)
     print(result[1].shape)
 
